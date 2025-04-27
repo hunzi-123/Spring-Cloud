83a350ff2c29a2cd2e0b4bf4dec1e0ab8d52ec4e
hunzi-123 <407137353@qq.com>
2025-04-27 17:35:57 +0800

    java练习

---
+++ eureka-hystrix/src/main/java/com/example/eurekahystrix/EurekaHystrixApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurekahystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaHystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaHystrixApplication.class, args);
+    }
+
+}
